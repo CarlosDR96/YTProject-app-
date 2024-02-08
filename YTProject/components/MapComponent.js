24ec1c89c43d75e398af929b7918ac026e23ee91
Joan Lopez <joan.lopez.salvans@gmail.com>
2024-02-08 09:45:34 +0100

    SP1-AP-H03-T017-MapComponent

--- YTProject/components/MapComponent.js
+++ YTProject/components/MapComponent.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
+
 
 const { width, height } = Dimensions.get('window');
 
 const MapComponent = () => {
   return (
-    <MapView
-      style={{ width: width, height: height * 0.6 }}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
   );
 };
 
+const styles = StyleSheet.create({
+  mapContainer: {
+    align: 'center',
+    flex: 1,
+    width: '95%',
+    height: '100%',
+    marginLeft: '2.5%',
+    padding: 20,
+    borderRadius: 15, // Ajusta este valor para cambiar la curvatura de las esquinas
+    overflow: 'hidden', // Importante para que las esquinas redondeadas se muestren correctamente
+  },
+  map: {
+    width: width,
+    height: height,
+    position: 'absolute',
+  },
+});
 export default MapComponent;
