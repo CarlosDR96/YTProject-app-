d9e8bc8447fdd52c7199973eed13524132b3d6e2
biel diaz <bieldiazbasullas@gmail.com>
2024-03-07 12:20:09 +0100

    07-03(5)

--- YTProject/App.js
+++ YTProject/App.js
@@ -7,7 +7,7 @@ import DetailsScreen from './screens/DetailsScreen';
 import SplashScreen from './screens/SplashScreen';
 import AboutScreen from './screens/AboutScreen';
 import PollScreen from './screens/PollScreen';
-import FavoritesScreen from './screens/FavoritesScreen'
+import FavoritesScreen from './screens/FavoritesScreen';
 
 
 const Stack = createStackNavigator();
@@ -16,10 +16,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" headerShown="false">
-        {/* SplashScreen se mostrará inicialmente */}
         <Stack.Screen name="Splash" component={SplashScreen}  options={{ headerShown: false }}/>
-
-        {/* HomeScreen se mostrará después de 10 segundos */}
         <Stack.Screen name="Home" component={HomeScreen}  options={{ headerShown: false }}/>
         <Stack.Screen name="Details" component={DetailsScreen}  options={{ animationEnabled: false, headerShown: false }}/>
         <Stack.Screen name="Poll" component={PollScreen} options={{ animationEnabled: false, headerShown: false }}/>
